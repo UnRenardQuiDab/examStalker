17ac3db89f1a4c4a26d9cc562d48ded3a1193e03
Bastien Wisniewski <bwisniew@student.42lyon.fr>
2024-10-11 04:16:16 +0200

    Initial test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Horse from './components/Horse';
+
+function App() {
+  return (
+    <div className="App">
+      <Horse />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Horse.js
@@ -0,0 +1,11 @@
+import './Horse.css';
+
+function Horse() {
+  return (
+    <div className="Horse">
+      
+    </div>
+  );
+}
+
+export default Horse;
